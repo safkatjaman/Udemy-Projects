fdc893fec8a7e580edbe5e88fe3e53a274ab9218
safkatjaman <19-40286-1@student.aiub.edu>
2024-02-28 01:33:19 +0600

    first day

---
+++ To Do List/To Do List/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+Console.WriteLine(userInput);
+
+userInput = "ABC";
+Console.WriteLine(userInput);
+
+int number;
+
+number = 70;
+Console.WriteLine(number);
+
+Console.ReadKey();
